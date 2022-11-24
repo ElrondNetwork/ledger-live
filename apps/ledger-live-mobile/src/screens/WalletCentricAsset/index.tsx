ea1785da82ce8666bc75898223f35cbb3ab0fa54
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-11-24 09:05:14 +0200

    remove console.log

--- apps/ledger-live-mobile/src/screens/WalletCentricAsset/index.tsx
+++ apps/ledger-live-mobile/src/screens/WalletCentricAsset/index.tsx
@@ -206,7 +206,6 @@ const AssetScreen = ({ route }: NavigationProps) => {
             <SectionContainer px={6} isLast>
               <SectionTitle title={t("analytics.operations.title")} />
               <OperationsHistorySection accounts={cryptoAccounts} />
-              {console.log(cryptoAccounts[0].operations)}
             </SectionContainer>,
           ]
         : []),
